d0cd8a8d569c2201cd7b37114b37dc5b6dacd010
Gerald Kim Ico <gerald_ico@launchlabs.ph>
2016-08-01 12:26:40 +0800

    project initialization

---
+++ FileHandling/FileHandling/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FileHandling
+//
+//  Created by LLDM on 01/08/2016.
+//  Copyright © 2016 LLDM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FileHandling/FileHandling/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FileHandling
+//
+//  Created by LLDM on 01/08/2016.
+//  Copyright © 2016 LLDM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
